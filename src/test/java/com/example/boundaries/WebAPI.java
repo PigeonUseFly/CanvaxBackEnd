04e75720f720ed7508d0c567a69d3cef957ccc40
HackerErmin <112625088+HackerErmin@users.noreply.github.com>
2023-05-01 14:42:19 +0200

    Revert "Fixade getJsonFile och skickar nu korrekt array av JSONobjekt till frontend"
    This reverts commit 7317fa31933a50615c17b36b5e5e92141a151ec6.

--- src/test/java/com/example/boundaries/WebAPI.java
+++ src/test/java/com/example/boundaries/WebAPI.java
@@ -22,7 +22,7 @@ public interface WebAPI {
 
     @ResponseBody
     @GetMapping(value = "/calendar/{ngt}")
-    ResponseEntity<Object> getJsonFile() throws IOException, JSONException, ParserException;
+    ResponseEntity<Object> getJsonFile() throws IOException, JSONException;
 
     @ResponseBody
     @GetMapping(value = "/programs/{id}")
