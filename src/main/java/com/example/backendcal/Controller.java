09c10928028267432104c48d56d6f8c934eaf963
TabascoTwan <115561387+TabascoTwan@users.noreply.github.com>
2023-05-06 15:40:15 +0200

    Skapade endpoint för att lägga till events. Fungerar men behöver formattering osv.

--- src/main/java/com/example/backendcal/Controller.java
+++ src/main/java/com/example/backendcal/Controller.java
@@ -2,16 +2,22 @@ package com.example.backendcal;
 
 import com.example.backendcal.boundaries.WebAPI;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import net.fortuna.ical4j.data.ParserException;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.*;
+import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -38,10 +44,31 @@ public class Controller implements WebAPI {
         iCalToJsonConverter.getObjectMapper().writeValue(new File("events.json"), iCalToJsonConverter.getParentObjectNode());
     }
 
+    public void insertEvent(String summary, String description, String startDateString, String endDateString, String location) throws IOException, ParseException {
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date startDate = formatter.parse(startDateString);
+        Date endDate = formatter.parse(endDateString);
+        EventJson eventJson = new EventJson(summary, description, startDate, endDate, location);
+        String eventData = mapper.writeValueAsString(eventJson);
+
+        FileWriter fileWriter = new FileWriter("events.json", true);
+        fileWriter.write(eventData);
+        fileWriter.close();
+    }
+
     @Configuration
+   // @ComponentScan(basePackages = {"com.example.backendcal"})
     public class WebConfig implements WebMvcConfigurer {
         public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
             converters.add(new GsonHttpMessageConverter());
+            converters.add(new FormHttpMessageConverter());
+        }
+
+        public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+            converters.stream()
+                    .filter(c -> c instanceof FormHttpMessageConverter)
+                    .forEach(c -> ((FormHttpMessageConverter) c).setCharset(StandardCharsets.UTF_8));
         }
     }
 }
--- src/main/java/com/example/backendcal/boundaries/WebAPI.java
+++ src/main/java/com/example/backendcal/boundaries/WebAPI.java
@@ -1,10 +1,15 @@
 package com.example.backendcal.boundaries;
 
+import com.example.backendcal.EventJson;
 import org.json.JSONException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.annotation.RequestScope;
+
 import java.io.IOException;
+import java.text.ParseException;
+import java.util.Date;
 
 @Validated
 public interface WebAPI {
@@ -15,4 +20,7 @@ public interface WebAPI {
 
     @PostMapping("/calendar/remove-element")
     void removeEvent(@RequestParam int index) throws IOException;
+
+    @PostMapping("/calendar/insert-element")
+    void insertEvent(@RequestParam String summary, @RequestParam String description, @RequestParam String startDate, @RequestParam String endDate, @RequestParam String location) throws IOException, ParseException;
 }
